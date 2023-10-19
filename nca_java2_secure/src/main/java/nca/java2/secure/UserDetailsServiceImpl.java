92f9978941e4350cc4618ef9bb8df4d54311b706
nca-kamite <2101050003kr@edu.nca.ac.jp>
2023-10-19 17:41:39 +0900

    更新

---
+++ nca_java2_secure/src/main/java/nca/java2/secure/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package nca.java2.secure;
+
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+@Transactional
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+	private final AccountsRepository accountsRepository;
+	public UserDetailsServiceImpl(AccountsRepository repository){
+		this.accountsRepository = repository;
+	}
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		AccountsEntity accounts = accountsRepository.findById(username).orElseThrow(() ->
+		new UsernameNotFoundException(username));
+		return User.withUsername(accounts.getUsername()).password(accounts.getPassword()).build();
+	}
+
+}
