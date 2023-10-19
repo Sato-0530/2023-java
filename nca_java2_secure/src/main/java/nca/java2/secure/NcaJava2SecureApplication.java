ca953eba361571c645755b353c3493c074c43839
nca-kamite <2101050003kr@edu.nca.ac.jp>
2023-10-19 16:21:33 +0900

    java授業

---
+++ nca_java2_secure/src/main/java/nca/java2/secure/NcaJava2SecureApplication.java
@@ -0,0 +1,13 @@
+package nca.java2.secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NcaJava2SecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NcaJava2SecureApplication.class, args);
+	}
+
+}
